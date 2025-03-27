8590c8242e54c476753ea430c8f04742ce314872
Massimosch <120001621+Massimosch@users.noreply.github.com>
2025-03-27 07:58:43 +0200

    State Machine For Player

--- Characters/GroundState.gd
+++ Characters/GroundState.gd
@@ -3,7 +3,12 @@ extends State
 class_name GroundState
 @export var jump_velocity : float = -200.0
 @export var air_state : State
+@export var jump_animation : String = ""
 
+
+func state_process(delta):
+	if (!character.is_on_floor()):
+		next_state = air_state
 
 func state_input(event : InputEvent):
 	if(event.is_action_pressed("jump")):
@@ -12,3 +17,4 @@ func state_input(event : InputEvent):
 func jump():
 	character.velocity.y = jump_velocity
 	next_state = air_state
+	playback.travel(jump_animation)
--- Characters/player.gd
+++ Characters/player.gd
@@ -1,20 +1,15 @@
 extends CharacterBody2D
 
-
 @export var speed : float = 200.0
-
-@export var double_jump_velocity : float = -100
-
 @onready var sprite : Sprite2D = $Sprite2D
 @onready var animation_tree : AnimationTree = $AnimationTree
 @onready var state_machine : CharacterStateMachine = $CharacterStateMachine
 
 #hae gravity projekti settingeistä ja synccc rigidbody nodejen kanssa
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var has_double_jumped : bool = false
-var animation_locked : bool = false
+
 var direction : Vector2 = Vector2.ZERO
-var was_in_air : bool = false
+
 
 func _ready():
 	#VOISI MYÖS VALITA ANIMATION TREESTÄ ACTIVE = ON
@@ -25,22 +20,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		was_in_air = true
-	else:
-		has_double_jumped = false
-		
-		if was_in_air == true:
-			land()
-			was_in_air = false
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			#Normal jump
-			pass
-		elif not has_double_jumped:
-			double_jump()
-			
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -51,10 +30,10 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
-	update_animation()
+	update_animation_parameters()
 	update_facing_dir()
 	
-func update_animation():
+func update_animation_parameters():
 	animation_tree.set("parameters/Move/blend_position", direction.x)
 
 		
@@ -63,12 +42,3 @@ func update_facing_dir():
 		sprite.flip_h = false
 	elif direction.x < 0:
 		sprite.flip_h = true
-
-func double_jump():
-	velocity.y = double_jump_velocity
-	animation_locked = true
-	has_double_jumped = true
-	
-func land():
-	animation_locked = true
-	
