74229db2bcc05c2c30c5cf31efebc555f425b82c
Mico <120001621+Massimosch@users.noreply.github.com>
2025-04-25 10:52:20 +0300

    removed ovani auto footsteps

--- addons/ovani_auto_footsteps/FootMatDetectionModule.gd
+++
@@ -1,6 +0,0 @@
-extends Object
-class_name FootMatDetectionModule
-
-func TryFigureMaterial(raycast_response : Dictionary, foot_node : AutoFootSteps) -> StringName:
-	assert(false)
-	return StringName("Not Implemented!")
